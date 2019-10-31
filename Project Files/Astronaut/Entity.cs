fb1b5c64594588ebaa73dd04572ee93ed66d625d
Domara Shlimon <domara2000@gmail.com>
2019-10-30 23:15:53 -0400

    added project

---
+++ Project Files/Astronaut/Entity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Astro {
+	abstract class Entity {
+
+
+
+		public virtual void Init() {
+
+		}
+
+		public virtual void Exit() {
+
+		}
+
+		public virtual void LoadContent() { }
+		public virtual void Update() { }
+		public virtual void Render() { }
+
+
+
+	}
+}
---
+++ Project Files/Astronaut/Vector2Extention.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Astro {
+	static class Vector2Extention {
+		public static Radial ToRadial(this Vector2 vector) {
+			return new Radial(vector);
+		}
+	}
+}
