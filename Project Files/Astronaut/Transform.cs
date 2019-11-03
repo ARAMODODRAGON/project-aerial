8864f675148f76d83420e39d7b60330bf9ece358
Domara Shlimon <domara2000@gmail.com>
2019-11-03 13:46:14 -0500

    Created Transform class

---
+++ Project Files/Astronaut/Transform.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Astro {
+	class Transform {
+		// reference to entity
+		public readonly Entity entity;
+
+		// world space data
+		public Vector2 position;
+		public Vector2 velocity;
+		public Vector2 scale;
+		public float RotInDeg;
+		public float RotInRad {
+			get => RotInDeg * BMath.DEGTORAD;
+			set => RotInDeg = value * BMath.RADTODEG;
+		}
+
+		public Transform(Entity entity) {
+			// set entity
+			this.entity = entity;
+
+			// set world space data
+			position = Vector2.Zero;
+			velocity = Vector2.Zero;
+			scale = Vector2.Zero;
+			RotInDeg = 0f;
+		}
+	}
+}
